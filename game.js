b5c2334a0058b2b23130bc54cdfe9494a36d3132
Drake Combs <hliquid.assassin@gmail.com>
2019-06-06 01:14:48 -0700

    added an array of images to the js file and some extra css

---
+++ game.js
@@ -0,0 +1,12 @@
+const mariochar = [ { Name: "Mario", image: 'Mario.png' }, 
+{ Name: "Luigi", image: 'Luigi.png' }, 
+{ Name: "Princess Peach", image: 'PrincessPeach.png' },
+{ Name: "King Boo", image: 'KingBoo.png' },
+{ Name: "Bowser", image: 'Bowser.png' },
+{ Name: "Wario", image: 'Wario.png' },
+{ Name: "Waluigi", image: 'Waluigi.png' },
+{ Name: "Toad", image: 'Toad.png' },
+{ Name: "Yoshi", image: 'Yoshi.png' },
+{ Name: "Birdo", image: 'Birdo.png' }
+];
+
